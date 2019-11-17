b81abb7b61ecb86f9ce8dcc628d7d15234423ea7
Ahaloua <ali.haloua@usmba.ac.ma>
2019-11-17 17:24:27 +0100

    fix abort issue

--- libft/ft_free_2d.c
+++ libft/ft_free_2d.c
@@ -6,7 +6,7 @@
 /*   By: ahaloua <ahaloua@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 00:53:40 by ahaloua           #+#    #+#             */
-/*   Updated: 2019/11/04 00:54:31 by ahaloua          ###   ########.fr       */
+/*   Updated: 2019/11/17 17:23:42 by ahaloua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,9 @@ void	ft_free_2d(char **free_me)
 	{
 		while (free_me[i])
 		{
-			ft_memdel((void **)&free_me[i]);
-			free(free_me[i]);
+			ft_strdel(&free_me[i]);
 			i += 0x1;
 		}
-		ft_memdel((void **)free_me);
-		free(free_me);
+		ft_strdel(free_me);
 	}
 }
